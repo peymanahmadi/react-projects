0e7994e13f8fdad42e75c6d84ae8428215f66431
Peyman Ahmadi <peyman.ahmadi@outlook.com>
2022-06-29 00:00:55 +0430

    [FIX]

--- 001-birthday-reminder/src/components/List.js
+++ 001-birthday-reminder/src/components/List.js
@@ -4,7 +4,7 @@ import "./List.css";
 const List = ({ people, clearHandle }) => {
   return (
     <div className="list-form">
-      <h1>{people.length} birthdays today</h1>
+      <h3>{people.length} birthdays today</h3>
       {people.map((person) => {
         return <SinglePerson key={person.id} {...person} />;
       })}
--- 001-birthday-reminder/src/components/SinglePerson.js
+++ 001-birthday-reminder/src/components/SinglePerson.js
@@ -2,14 +2,23 @@ import "./SinglePerson.css";
 
 const SinglePerson = ({ image, name, age }) => {
   return (
-    <div className="person">
+    <article className="person">
       <img src={image} alt={name} />
       <div>
-        <h2>{name}</h2>
+        <h4>{name}</h4>
         <p>{age} years</p>
       </div>
-    </div>
+    </article>
   );
 };
 
 export default SinglePerson;
+
+// the content within the <article> tag is independent of the other content of the site (even though it can be related).
+// Potential sources for the <article> element:
+
+// Forum post
+// Blog post
+// News story
+
+// Default CSS Settings: display: block;
